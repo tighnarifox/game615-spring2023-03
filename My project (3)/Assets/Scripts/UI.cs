0eec213f5f8907a5708d78b8e114f42097b8d146
TASallin <tristansallin@yahoo.com>
2023-02-08 20:27:47 -0500

    build complete

--- My project (3)/Assets/Scripts/UI.cs
+++ My project (3)/Assets/Scripts/UI.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class UI : MonoBehaviour
-{
+public class UI : MonoBehaviour {
 
     public PlayerController pc; //ui must see the player controller to display the score
     public TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI winDisplay;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        winDisplay.text = "";
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (pc.score >= 8) { //has collected all the pizzas
+            winDisplay.text = "You Win!";
+        } else if (pc.lose) {
+            winDisplay.text = "You Lose!";
+        }
         scoreDisplay.text = "Score: " + pc.score;
     }
 }
